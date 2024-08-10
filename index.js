ff0728024b10b437e81f9406b0056de4b7816aa7
SAILENDRA KONDRAJU <kondrajusailendra@gmail.com>
2024-08-10 13:29:35 +0530

    third commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: ["https://deploy-frontend-green.vercel.app"],methods:["GET","POST","PUT","DELETE"],credentials:true}));
 app.use(express.json());
 
 // Routes
